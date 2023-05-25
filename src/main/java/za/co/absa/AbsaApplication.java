88cd192a018d829168a3f48c0952ee88f1d5d26d
Pavel Teroschin <pteroschin@gmail.com>
2023-05-25 12:22:11 +0200

    Initial commit

---
+++ src/main/java/za/co/absa/AbsaApplication.java
@@ -0,0 +1,13 @@
+package za.co.absa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsaApplication.class, args);
+	}
+
+}
