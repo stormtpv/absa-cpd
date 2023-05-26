6bcb1f598539bab786f6372f1ef4f3f550c9b9c4
Pavel Teroschin <pteroschin@gmail.com>
2023-05-26 21:23:14 +0200

    adjust custom response dto

--- src/main/java/za/co/absa/model/dto/customer/CustomerResponseDto.java
+++ src/main/java/za/co/absa/model/dto/customer/CustomerResponseDto.java
@@ -1,10 +1,15 @@
 package za.co.absa.model.dto.customer;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import za.co.absa.model.dto.purchase.PurchaseDto;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CustomerResponseDto {
 
     private Long id;
@@ -14,4 +19,6 @@ public class CustomerResponseDto {
     private LocalDate birthDate;
 
     private String address;
+
+    private List<PurchaseDto> purchases = new ArrayList<>();
 }
